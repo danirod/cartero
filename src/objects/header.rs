4dfe7c6bbf3b71495363944e1d66f3dde8bc96ec
Dani Rodríguez <dani@danirod.es>
2024-04-13 13:38:34 +0200

    Fix tests and remove unused code for Header

--- src/objects/header.rs
+++ src/objects/header.rs
@@ -1,11 +1,28 @@
+// Copyright 2024 the Cartero authors
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+//
+// SPDX-License-Identifier: GPL-3.0-or-later
+
 use glib::Object;
 
 mod imp {
     use std::cell::RefCell;
 
-    use glib::object::ObjectExt;
     use glib::Properties;
     use gtk4::glib;
+    use gtk4::glib::prelude::*;
     use gtk4::glib::subclass::prelude::*;
 
     #[derive(Default, Debug, Properties)]
@@ -13,10 +30,8 @@ mod imp {
     pub struct Header {
         #[property(get, set)]
         active: RefCell<bool>,
-
         #[property(get, set)]
         header_name: RefCell<String>,
-
         #[property(get, set)]
         header_value: RefCell<String>,
     }
@@ -28,34 +43,11 @@ mod imp {
     }
 
     #[glib::derived_properties]
-    impl ObjectImpl for Header {
-        fn constructed(&self) {
-            self.parent_constructed();
-        }
-    }
+    impl ObjectImpl for Header {}
 }
 
 glib::wrapper! {
     pub struct Header(ObjectSubclass<imp::Header>);
-}
-
-impl Header {
-    pub fn new(name: &str, value: &str) -> Self {
-        Object::builder()
-            .property("header-name", name)
-            .property("header-value", value)
-            .property("active", true)
-            .build()
-    }
-
-    pub fn print(&self) {
-        println!(
-            "[{}] {}: {}",
-            self.active(),
-            self.header_name(),
-            self.header_value()
-        );
-    }
 }
 
 impl Default for Header {
@@ -69,16 +61,22 @@ mod tests {
     use super::Header;
 
     #[test]
-    pub fn test_this_works() {
-        let header = Header::new("Accept", "text/html");
-        assert_eq!(header.header_name(), "Accept");
-        assert_eq!(header.header_value(), "text/html");
+    pub fn test_header_properties() {
+        let header = Header::default();
+        assert_eq!(header.header_name(), "");
+        assert_eq!(header.header_value(), "");
         assert!(!header.active());
 
         header.set_header_name("Content-Type");
-        assert_ne!(header.header_name(), "Accept");
-        assert_eq!(header.header_name(), "Content-Type");
+        header.set_header_value("text/plain");
         header.set_active(true);
+        assert_eq!(header.header_name(), "Content-Type");
+        assert_eq!(header.header_value(), "text/plain");
+        assert!(header.active());
+
+        header.set_header_name("Accept");
+        assert_eq!(header.header_name(), "Accept");
+        assert_eq!(header.header_value(), "text/plain");
         assert!(header.active());
     }
 }

d7cfa90b81255baca413cea5ab72c6f69ab87251
Dani Rodríguez <dani@danirod.es>
2024-07-26 00:17:14 +0200

    Use last known locations in file dialogs
    Whenever a new file gets opened or saved, the directory where the file
    is present will be stored, so that the next time the dialog is summoned,
    that will be the initial folder.

--- src/widgets/file_dialogs.rs
+++ src/widgets/file_dialogs.rs
@@ -1,9 +1,13 @@
 use gettextrs::gettext;
 use glib::types::StaticType;
-use gtk::{gio::ListStore, prelude::FileExt, DialogError, FileDialog, FileFilter};
+use gtk::{
+    gio::ListStore,
+    prelude::{FileExt, SettingsExtManual},
+    DialogError, FileDialog, FileFilter,
+};
 use std::path::PathBuf;
 
-use crate::{error::CarteroError, win::CarteroWindow};
+use crate::{app::CarteroApplication, error::CarteroError, win::CarteroWindow};
 
 fn get_cartero_file_filter() -> FileFilter {
     let filter = FileFilter::new();
@@ -31,6 +35,15 @@ pub async fn open_file(win: &CarteroWindow) -> Result<PathBuf, CarteroError> {
         .default_filter(&cartero)
         .modal(true)
         .build();
+
+    let app = CarteroApplication::get();
+    let settings = app.settings();
+    if let Some(dir) = settings.get::<Option<String>>("last-open-dir") {
+        let path = PathBuf::from(&dir);
+        let file = gtk::gio::File::for_path(path);
+        dialog.set_initial_folder(Some(&file));
+    }
+
     let file = dialog.open_future(Some(win)).await.map_err(|e| {
         if let Some(file_error) = e.kind::<DialogError>() {
             match file_error {
@@ -42,6 +55,12 @@ pub async fn open_file(win: &CarteroWindow) -> Result<PathBuf, CarteroError> {
         }
     })?;
     let path = file.path().ok_or(CarteroError::FileDialogError)?;
+    if let Some(folder) = path.parent() {
+        let location = folder.to_str().ok_or(CarteroError::FileDialogError)?;
+        settings
+            .set("last-open-dir", Some(location))
+            .map_err(|_| CarteroError::FileDialogError)?;
+    }
     Ok(path)
 }
 
@@ -58,6 +77,15 @@ pub async fn save_file(win: &CarteroWindow) -> Result<PathBuf, CarteroError> {
         .default_filter(&cartero)
         .initial_name("request.cartero")
         .build();
+
+    let app = CarteroApplication::get();
+    let settings = app.settings();
+    if let Some(dir) = settings.get::<Option<String>>("last-save-dir") {
+        let path = PathBuf::from(&dir);
+        let file = gtk::gio::File::for_path(path);
+        dialog.set_initial_folder(Some(&file));
+    }
+
     let file = dialog.save_future(Some(win)).await.map_err(|e| {
         if let Some(file_error) = e.kind::<DialogError>() {
             match file_error {
@@ -69,5 +97,11 @@ pub async fn save_file(win: &CarteroWindow) -> Result<PathBuf, CarteroError> {
         }
     })?;
     let path = file.path().ok_or(CarteroError::FileDialogError)?;
+    if let Some(folder) = path.parent() {
+        let location = folder.to_str().ok_or(CarteroError::FileDialogError)?;
+        settings
+            .set("last-save-dir", Some(location))
+            .map_err(|_| CarteroError::FileDialogError)?;
+    }
     Ok(path)
 }

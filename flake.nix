ed0a1d83cedd00726cf6641edd6d7fafcba774a0
Dani Rodríguez <dani@danirod.es>
2024-07-30 15:01:15 +0200

    Remove dots from the application summary
    This is a warning on appstreamcli, so I'd rather just normalize and
    remove every ocurrence of a dot after the sentence "Make HTTP requests
    and test APIs". I've update the translations as well since this change
    affects them and marks them fuzzy.

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "Make HTTP requests and test APIs.";
+  description = "Make HTTP requests and test APIs";
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";

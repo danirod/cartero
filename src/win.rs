ddf9899eacb3f281ae50248321057904251f8d1e
Dani Rodríguez <dani@danirod.es>
2025-03-16 19:44:13 +0100

    Update the copyright year in the about dialog
    I never changed and I have been releasing a couple of versions during
    this year. So, change the copyright year now that it can be considered
    that the list of changes is exhaustive.

--- src/win.rs
+++ src/win.rs
@@ -703,7 +703,7 @@ mod imp {
                             .issue_url("https://github.com/danirod/cartero/issues")
                             .support_url("https://github.com/danirod/cartero/discussions")
                             .developer_name(gettext("The Cartero authors"))
-                            .copyright(gettext("© 2024 the Cartero authors"))
+                            .copyright(gettext("© 2024-2025 the Cartero authors"))
                             .license_type(gtk::License::Gpl30)
                             .build();
                         if cfg!(target_os = "macos") {

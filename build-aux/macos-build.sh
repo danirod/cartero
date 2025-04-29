89bdf2c53b0be46f97f5ce15eed0eb8297af1b95
Dani Rodríguez <dani@danirod.es>
2025-04-29 22:15:41 +0200

    Release 0.2.2

--- build-aux/macos-build.sh
+++ build-aux/macos-build.sh
@@ -8,13 +8,13 @@ case "$1" in
   devel)
     BUNDLE_ID="es.danirod.Cartero.Devel"
     APP_NAME="Cartero (Devel)"
-    APP_VERSION="0.2.1"
+    APP_VERSION="0.2.2"
     MESON_FLAGS="$MESON_FLAGS -Dprofile=development"
     ;;
   stable)
     BUNDLE_ID="es.danirod.Cartero"
     APP_NAME="Cartero"
-    APP_VERSION="0.2.1"
+    APP_VERSION="0.2.2"
     MESON_FLAGS="$MESON_FLAGS -Dprofile=default"
     ;;
   *)

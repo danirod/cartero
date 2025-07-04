326c478555dd576fc3d6eb0cd2c4708b766f8e69
Dani Rodríguez <dani@danirod.es>
2025-07-04 06:34:37 +0200

    Fix double hash in version number
    Apparently the meson.build already sets the version code with the
    SHA-ref. Therefore, it is not necessary to add it on
    gen-nightly-version.sh.

--- build-aux/gen-nightly-version.sh
+++ build-aux/gen-nightly-version.sh
@@ -4,10 +4,4 @@ cd "$(dirname "$0")/.."
 
 VERSION=$(cargo metadata --format-version=1 --no-deps | jq -r '.packages[] | select(.name == "cartero") | .version')
 DATE=$(date +%Y%m%d)
-
-if [ -d .git ]; then
-    HASH=$(git rev-parse --short HEAD)
-    echo $VERSION-nightly-$DATE-$HASH
-else
-    echo $VERSION-nightly-$DATE
-fi
+echo $VERSION-nightly-$DATE

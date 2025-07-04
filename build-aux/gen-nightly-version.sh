0ed5cad913cddb354b783041f75872f201b6b650
Dani Rodríguez <dani@danirod.es>
2025-07-04 17:53:56 +0200

    Portable version number generator

--- build-aux/gen-nightly-version.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-cd "$(dirname "$0")/.."
-
-VERSION=$(cargo metadata --format-version=1 --no-deps | jq -r '.packages[] | select(.name == "cartero") | .version')
-DATE=$(date +%Y%m%d)
-echo $VERSION-nightly-$DATE

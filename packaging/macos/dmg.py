7c3c990e2ad5c1eecd706dee0065c57ac559004e
Dani Rodríguez <dani@danirod.es>
2025-08-01 16:08:10 +0200

    Provide valid SPDX identifiers through the repo

--- packaging/macos/dmg.py
+++ packaging/macos/dmg.py
@@ -1,4 +1,20 @@
 #!/usr/bin/env python3
+# Copyright 2024-2025 the Cartero authors
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
 
 # This is the script that deals with signing the app.
 #
@@ -50,7 +66,7 @@ def create_dmg(path: Path):
         "640",
         "225",
         "--eula",
-        self_dir / ".." / "GPL-3.0.rtf",
+        self_dir / ".." / "COPYING.rtf",
         path / "Cartero.dmg",
         path,
     ]

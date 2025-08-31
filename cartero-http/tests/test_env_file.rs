a8080b49e8140a217df8bef6ba2acc06f3774fd6
Dani Rodríguez <dani@danirod.es>
2025-08-31 14:17:42 +0200

    Address test failures and reuse issues

--- cartero-http/tests/test_env_file.rs
+++ cartero-http/tests/test_env_file.rs
@@ -1,3 +1,20 @@
+// Copyright 2024-2025 the Cartero authors
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+//
+// SPDX-License-Identifier: GPL-3.0-or-later
+
 use std::path::PathBuf;
 
 use cartero_http::{BoundRequest, ClientConfig, RequestEnvironment};
@@ -29,6 +46,7 @@ async fn test_read_env_vars() {
     let env_file = EnvFile::builder().file(Some(&env_file_src)).build();
     let env = RequestEnvironment {
         prefix: None,
+        proxy: None,
         env_file: Some(env_file),
         config: ClientConfig {
             validate_tls: false,

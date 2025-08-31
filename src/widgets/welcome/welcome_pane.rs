2010c0755f2c205648986418b6f36f99572eaa10
Dani Rodríguez <dani@danirod.es>
2025-08-31 18:03:46 +0200

    Add GPL and links card in the bottom

--- src/widgets/welcome/welcome_pane.rs
+++ src/widgets/welcome/welcome_pane.rs
@@ -19,22 +19,39 @@ use adw::prelude::*;
 use adw::subclass::prelude::*;
 
 mod imp {
+    use std::cell::RefCell;
+
     use super::*;
     use glib::subclass::InitializingObject;
-    use gtk::CompositeTemplate;
+    use gtk::{
+        gio::{SimpleAction, SimpleActionGroup},
+        CompositeTemplate,
+    };
 
     #[derive(Default, CompositeTemplate)]
     #[template(resource = "/es/danirod/Cartero/welcome/welcome_pane.ui")]
-    pub struct WelcomePane {}
+    pub struct WelcomePane {
+        #[template_child]
+        rights: TemplateChild<adw::Dialog>,
+
+        carousel_position: RefCell<u32>,
+        #[template_child]
+        carousel: TemplateChild<adw::Carousel>,
+        #[template_child]
+        previous: TemplateChild<gtk::Button>,
+        #[template_child]
+        next: TemplateChild<gtk::Button>,
+    }
 
     #[glib::object_subclass]
     impl ObjectSubclass for WelcomePane {
         const NAME: &'static str = "CarteroWelcomePane";
         type Type = super::WelcomePane;
-        type ParentType = adw::Bin;
+        type ParentType = adw::BreakpointBin;
 
         fn class_init(klass: &mut Self::Class) {
             klass.bind_template();
+            klass.bind_template_callbacks();
         }
 
         fn instance_init(obj: &InitializingObject<Self>) {
@@ -42,16 +59,82 @@ mod imp {
         }
     }
 
-    impl ObjectImpl for WelcomePane {}
+    impl ObjectImpl for WelcomePane {
+        fn constructed(&self) {
+            self.parent_constructed();
+
+            let action_rights = SimpleAction::new("license-conditions", None);
+            action_rights.connect_activate(glib::clone!(
+                #[weak(rename_to = imp)]
+                self,
+                move |_, _| {
+                    imp.reset_license_tour();
+                }
+            ));
+
+            let action_group = SimpleActionGroup::new();
+            action_group.add_action(&action_rights);
+            self.obj()
+                .insert_action_group("widget", Some(&action_group));
+        }
+    }
 
     impl WidgetImpl for WelcomePane {}
 
-    impl BinImpl for WelcomePane {}
+    impl BreakpointBinImpl for WelcomePane {}
+
+    #[gtk::template_callbacks]
+    impl WelcomePane {
+        fn reset_license_tour(&self) {
+            self.carousel_position.replace(0);
+            let page = self.carousel.nth_page(0);
+            self.carousel.scroll_to(&page, false);
+            self.update_buttons();
+            self.rights.present(Some(&*self.obj()));
+        }
+
+        #[template_callback]
+        fn next_page(&self) {
+            let pos = *self.carousel_position.borrow();
+            if pos <= self.carousel.n_pages() - 2 {
+                self.switch_page(pos + 1);
+            }
+        }
+
+        #[template_callback]
+        fn previous_page(&self) {
+            let pos = *self.carousel_position.borrow();
+            if pos > 0 {
+                self.switch_page(pos - 1);
+            }
+        }
+
+        fn switch_page(&self, pos: u32) {
+            let page = self.carousel.nth_page(pos);
+            self.carousel.scroll_to(&page, true);
+            self.carousel_position.replace(pos);
+            self.update_buttons();
+        }
+
+        fn update_buttons(&self) {
+            let pos = *self.carousel_position.borrow();
+            let is_first = pos == 0;
+            let is_last = pos == self.carousel.n_pages() - 1;
+            self.next.set_sensitive(!is_last);
+            self.previous.set_sensitive(!is_first);
+
+            if is_first {
+                self.next.add_css_class("suggested-action");
+            } else {
+                self.next.remove_css_class("suggested-action");
+            }
+        }
+    }
 }
 
 glib::wrapper! {
     pub struct WelcomePane(ObjectSubclass<imp::WelcomePane>)
-        @extends gtk::Widget, adw::Bin,
+        @extends gtk::Widget, adw::BreakpointBin,
         @implements gtk::Accessible, gtk::Buildable, gtk::ConstraintTarget;
 }
 

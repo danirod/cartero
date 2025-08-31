2a707c2c1a1be184bb9a253e97bb935ef8c79f04
Dani Rodríguez <dani@danirod.es>
2025-08-31 14:56:39 +0200

    Unify the welcome page in a separate widget
    No more duplication!

---
+++ src/widgets/welcome/welcome_pane.rs
@@ -0,0 +1,62 @@
+// Copyright 2024-2025 the Cartero authors
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+//
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+use adw::prelude::*;
+use adw::subclass::prelude::*;
+
+mod imp {
+    use super::*;
+    use glib::subclass::InitializingObject;
+    use gtk::CompositeTemplate;
+
+    #[derive(Default, CompositeTemplate)]
+    #[template(resource = "/es/danirod/Cartero/welcome/welcome_pane.ui")]
+    pub struct WelcomePane {}
+
+    #[glib::object_subclass]
+    impl ObjectSubclass for WelcomePane {
+        const NAME: &'static str = "CarteroWelcomePane";
+        type Type = super::WelcomePane;
+        type ParentType = adw::Bin;
+
+        fn class_init(klass: &mut Self::Class) {
+            klass.bind_template();
+        }
+
+        fn instance_init(obj: &InitializingObject<Self>) {
+            obj.init_template();
+        }
+    }
+
+    impl ObjectImpl for WelcomePane {}
+
+    impl WidgetImpl for WelcomePane {}
+
+    impl BinImpl for WelcomePane {}
+}
+
+glib::wrapper! {
+    pub struct WelcomePane(ObjectSubclass<imp::WelcomePane>)
+        @extends gtk::Widget, adw::Bin,
+        @implements gtk::Accessible, gtk::Buildable, gtk::ConstraintTarget;
+}
+
+impl Default for WelcomePane {
+    fn default() -> Self {
+        glib::Object::new()
+    }
+}

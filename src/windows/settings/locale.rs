c0e03c3cc9e2560d831b757d3ea3837cb34bcdcf
Dani Rodríguez <dani@danirod.es>
2025-11-10 22:53:12 +0100

    Update language pickers
    Recently, Indonesian and Ukranian were added as locale languages, so it
    is time to add these to the languages table. The instructions so far
    when a new language is added are:
    
    - Sort again the LINGUAS file, because Weblate likes to add the new
      language to the last line, which will break the CI pipeline.
    - Add the language to the src/windows/settings/locale.rs, to update
      the language picker for the settings dialog.
    - Download a translation for the Windows installer at
      https://jrsoftware.org/files/istrans/.
    
    Other actions taken:
    
    - Fix a typo for Euskara.
    - Some languages did not have a translation.

--- src/windows/settings/locale.rs
+++ src/windows/settings/locale.rs
@@ -18,20 +18,22 @@
 use gettextrs::gettext;
 use gtk::gio::ListStore;
 
-const LOCALES: [(&str, &str); 13] = [
+const LOCALES: [(&str, &str); 15] = [
     ("ca", "Català"),
     ("cs", "Čeština"),
     ("de", "Deutsch"),
     ("eo", "Esperanto"),
     ("es", "Español"),
-    ("eu", "Euskera"),
+    ("eu", "Euskara"),
     ("fr", "Français"),
     ("gl", "Galego"),
+    ("id", "Bahasa Indonesia"),
     ("pt", "Português"),
     ("pt_BR", "Português (Brasil)"),
     ("ro", "Română"),
     ("ru", "Русский язык"),
     ("ta", "தமிழ்"),
+    ("uk", "Українська"),
 ];
 
 use glib::subclass::prelude::*;

cb6badfc00bf65bc96a2d7c9c96822608eb03741
Dani Rodríguez <dani@danirod.es>
2025-11-17 00:02:26 +0100

    Read CodeView theme from the settings

--- src/widgets/code_view.rs
+++ src/widgets/code_view.rs
@@ -252,8 +252,12 @@ mod imp {
         fn update_source_view_style(&self) {
             let obj = self.obj();
             let dark_mode = adw::StyleManager::default().is_dark();
-            let color_theme = if dark_mode { "Adwaita-dark" } else { "Adwaita" };
-            let theme = StyleSchemeManager::default().scheme(color_theme);
+            let color_theme = if dark_mode {
+                self.settings.get::<String>("color-scheme-dark")
+            } else {
+                self.settings.get::<String>("color-scheme-light")
+            };
+            let theme = StyleSchemeManager::default().scheme(&color_theme);
             let buffer = obj.buffer().downcast::<sourceview5::Buffer>().unwrap();
             match theme {
                 Some(theme) => {

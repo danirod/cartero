bc48f9dee11ddbfe704c95ff94245f5dfb5979f6
Dani Rodríguez <dani@danirod.es>
2026-01-02 23:53:01 +0100

    Update copyright

--- website/src/consts.ts
+++ website/src/consts.ts
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: GPL-3.0-or-later OR CC-BY-SA-4.0
-// SPDX-FileCopyrightText: 2024-2025 the Cartero authors
+// SPDX-FileCopyrightText: 2024-2026 the Cartero authors
 
 export const SITE_TITLE = "Cartero";
 export const SITE_DESCRIPTION = "Make HTTP requests and test APIs";

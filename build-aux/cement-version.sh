f3d00069731aead9ab308fb475ef6b4676caeb74
Dani Rodríguez <dani@danirod.es>
2026-01-24 21:06:25 +0100

    Validate the output of the Snapcraft package
    This is the first step towards an unified infrastructure to build
    development and release Snapcraft packages.

--- build-aux/cement-version.sh
+++ build-aux/cement-version.sh
@@ -5,4 +5,9 @@
 set -eu
 
 # NOTE: this command will fail unless meson >= 1.4.0
-$MESONREWRITE -V --sourcedir="$MESON_PROJECT_DIST_ROOT" kwargs set project / version "$1"
+$MESONREWRITE -V --sourcedir="$MESON_PROJECT_DIST_ROOT" kwargs set project / version "$2"
+
+# There is static data in the snapcraft.yaml package too, so it is time to rewrite.
+sed -i -e "s/version: .*/version: '$2'/" \
+    -i -e "s/es.danirod.Cartero.Devel/$1/g" \
+    "$MESON_PROJECT_DIST_ROOT/snapcraft.yaml"

e865525c6dcd0e1f3f7cc8bfcf9d8def405cc8fe
youpie <youplamb@hotmail.nl>
2026-01-25 13:33:53 +0100

    added help-overlay action

--- src/win.rs
+++ src/win.rs
@@ -652,6 +652,23 @@ mod imp {
             about.present(Some(&*obj));
         }
 
+        fn action_help_overlay(&self) {
+            let obj = self.obj();
+            let dialog = gtk::Builder::from_resource("/es/danirod/Cartero/gtk/help-overlay.ui")
+                .object::<adw::ShortcutsDialog>("shortcuts_dialog")
+                .expect("Could not build help-overlay");
+
+            dialog.connect_closed(glib::clone!(
+                #[weak]
+                obj,
+                move |_| {
+                    obj.present();
+                }
+            ));
+
+            dialog.present(Some(&*obj));
+        }
+
         fn update_native_appearance(&self) {
             let gtk_window = self.obj().clone().upcast::<gtk::Window>();
             let headerbar_height = match self.toolbar.top_bar_style() {
@@ -892,6 +909,16 @@ mod imp {
                 ))
                 .build();
 
+            let action_help_overlay = ActionEntry::builder("show-help-overlay")
+                .activate(glib::clone!(
+                    #[weak(rename_to = window)]
+                    self,
+                    move |_, _, _| {
+                        window.action_help_overlay();
+                    }
+                ))
+                .build();
+
             let action_export_request = ActionEntry::builder("export-request")
                 .parameter_type(Some(&String::static_variant_type()))
                 .activate(glib::clone!(
@@ -927,6 +954,7 @@ mod imp {
                 action_duplicate,
                 action_close,
                 action_about,
+                action_help_overlay,
                 action_export_request,
                 action_export_response_body,
             ]);
